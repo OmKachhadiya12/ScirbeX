afd8e74f9e5cf373542e7d7630a1b96a90b4394f
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-04-15 11:51:02 +0530

    Add the all Pages and Components and all required lib , Utils.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI';
+import { TrendingUpDown } from 'lucide-react';
+import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard";
+import api from '../lib/axios';
+import NotesnotFound from '../components/NotesnotFound';
 
 const HomePage = () => {
+
+  const [isRateLimited,setisRateLimited] = useState(false);
+  const [notes,setNotes] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async() => {
+        try {
+
+        const res = await api.get("/notes");
+        console.log(res.data);
+        setNotes(res.data);
+        setisRateLimited(false);
+
+      } catch (error) {
+
+        console.log("Error in Fetching!!!");
+        if(error.response?.status == 429) {
+          setisRateLimited(true);
+        } else {
+          toast.error("Failed to Load Notes.");
+        }
+
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+    
+  },[]);
+
   return (
-    <div>
-      HomePage
+
+    <div className="min-h-screen">
+      <Navbar/>
+
+      {isRateLimited && <RateLimitedUI/>}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {notes.length === 0 && !isRateLimited && <NotesnotFound />}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
+            ))}
+          </div>
+        )}
+      </div>
+
     </div>
+
   )
 }
 
